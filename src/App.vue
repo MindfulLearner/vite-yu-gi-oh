87abccfd524aa32b828f15e31e060fcd00fd9e11
MindfulLearner <dimajosh1009@gmail.com>
2024-09-13 21:07:22 +0000

    init love of coding

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+    import Header from './components/Header.vue';
+    import Main from './components/Main.vue';
+    export default {
+        data() {
+            return {
+                count: 0,
+            }
+        },
+        components: {
+            Header,
+            Main
+        }
+    }
+</script>
+
+<template>
+    <Header />
+    <Main />
+</template>
+
+<style scoped>
+</style>
